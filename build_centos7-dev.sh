8646fe3136df7ddfa9103ef0048b37fed43a5480
Jeffrey Zhang <zhang.lei.fly@gmail.com>
2019-05-28 14:23:41 +0800

    add build centos7 dev image

---
+++ build_centos7-dev.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+export DIB_IMAGE_SIZE=2
+
+export ELEMENTS_PATH=elements
+#export TMP_BUILD_DIR=/data/dib
+
+#export DIB_CLOUD_IMAGES=http://images.trystack.cn/centos
+#export BASE_IMAGE_FILE=CentOS-7-x86_64-GenericCloud.qcow2.xz
+export DIB_DISTRIBUTION_MIRROR=http://mirrors.aliyun.com/centos
+# Only support ConfigDrive which is faster
+export DIB_CLOUD_INIT_DATASOURCES="ConfigDrive"
+export DIB_EPEL_MIRROR=http://mirrors.aliyun.com/epel
+
+export DIB_CLOUD_INIT_ALLOW_SSH_PWAUTH=1
+export DIB_CLOUD_INIT_ENABLE_ROOT=1
+
+export DIB_DEV_USER_USERNAME=jeffrey4l
+export DIB_DEV_USER_PASSWORD=password
+export DIB_DEV_USER_PWDLESS_SUDO=1
+
+export FS_TYPE=xfs
+
+disk-image-create -a amd64 -o centos7-amd64 -t qcow2 "$@" \
+    auto-fstrim \
+    centos7 \
+    cloud-init \
+    cloud-init-datasources \
+    devuser \
+    dhcp-all-interfaces \
+    disable-selinux \
+    element-manifest \
+    epel \
+    openssh-config \
+    qemu-guest-agent \
+    timezone \
+    vm
